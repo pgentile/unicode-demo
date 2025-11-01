5d4aa604f54e67b87ed8187380604d90bf19cef5
Pierre Gentile <pierre.gentile.perso@gmail.com>
2025-11-01 21:49:15 +0100

    feat: better display of sample values

--- src/SampleValueGroup.tsx
+++ src/SampleValueGroup.tsx
@@ -1,56 +1,81 @@
-import SampleValue from "./SampleValue.tsx";
+import { useSource } from "./SourceContextBase.ts";
+import type { MouseEvent } from "react";
+
+const SAMPLE_VALUES: readonly string[] = [
+  "ABC",
+  "Hélice",
+  "Hélice", // Example with combining character for the é
+  "ﬃ",
+  "😱",
+  "🧑‍🧑‍🧒‍🧒",
+  "🇺🇸",
+  "é",
+  "é",
+  "👍",
+  "👍🏽",
+  "Hello Tech Day",
+  "Ǚ",
+  "𝕭𝖔𝖔𝖒",
+  "œ",
+  "ß",
+  "①②③",
+  "◌̈",
+  "🙋‍♂️",
+  "👩🏾‍🦳",
+  "🇫🇷",
+  "🏳️‍🌈",
+  "🏴‍☠️",
+  "👩‍🍼",
+  "👩🏻‍🍼",
+  "💇🏻‍♂️",
+  "🏄🏽‍♀️",
+  "🏃🏿‍♂️",
+  "⚽️",
+  "↗️",
+  "❤️‍🔥",
+  "🥇",
+  "ﬁ",
+  "Україна",
+  "불",
+  "불",
+  "자모",
+  "㍍㌔㌢",
+  "が",
+  "ئ",
+  "בּ",
+  "𓀃",
+];
 
 export default function SampleValueGroup() {
-  const sampleValues = [
-    "ABC",
-    "Hélice",
-    "Hélice", // Example with combining character for the é
-    "ﬃ",
-    "😱",
-    "🧑‍🧑‍🧒‍🧒",
-    "🇺🇸",
-    "é",
-    "é",
-    "👍",
-    "👍🏽",
-    "Hello Tech Day",
-    "Ǚ",
-    "𝕭𝖔𝖔𝖒",
-    "œ",
-    "ß",
-    "①②③",
-    "◌̈",
-    "🙋‍♂️",
-    "👩🏾‍🦳",
-    "🇫🇷",
-    "🏳️‍🌈",
-    "🏴‍☠️",
-    "👩‍🍼",
-    "👩🏻‍🍼",
-    "💇🏻‍♂️",
-    "↗️",
-    "❤️‍🔥",
-    "🥇",
-    "ﬁ",
-    "Україна",
-    "불",
-    "불",
-    "자모",
-    "㍍㌔㌢",
-    "が",
-    "ئ",
-    "בּ",
-    "𓀃",
-  ];
-
   return (
     <div className="source-samples">
-      <p>Please try with the following examples:</p>
-      <p className="samples">
-        {sampleValues.map((sampleValue) => (
-          <SampleValue key={sampleValue} sampleValue={sampleValue} />
+      <p className="please-try">Please try with the following examples:</p>
+      <ul className="samples">
+        {SAMPLE_VALUES.map((sampleValue, index) => (
+          <SampleValue key={index} sampleValue={sampleValue} />
         ))}
-      </p>
+      </ul>
     </div>
+  );
+}
+
+interface SampleValueProps {
+  sampleValue: string;
+}
+
+function SampleValue({ sampleValue }: SampleValueProps) {
+  const [, setSource] = useSource();
+
+  const onSampleDataClick = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    setSource(sampleValue);
+  };
+
+  return (
+    <li className="sample-value">
+      <a href="#" onClick={onSampleDataClick}>
+        {sampleValue}
+      </a>
+    </li>
   );
 }
