f76125e96206acfeeab472bb5200bbda299649d9
Pierre Gentile <pierre.gentile.perso@gmail.com>
2025-11-01 09:57:19 +0100

    feat: more character info: uppercase and lowercase mappings

--- src/CharacterInfo.tsx
+++ src/CharacterInfo.tsx
@@ -40,10 +40,39 @@ const bidiClassesPromise: Promise<Map<number, string>> = new Promise(
   },
 );
 
+const lowercaseMappingPromise: Promise<Map<number, number>> = new Promise(
+  (resolve, reject) => {
+    async function loadModule() {
+      const module = await import(
+        "@unicode/unicode-17.0.0/Simple_Case_Mapping/Lowercase/code-points"
+      );
+      return module.default;
+    }
+
+    loadModule().then(resolve, reject);
+  },
+);
+
+const uppercaseMappingPromise: Promise<Map<number, number>> = new Promise(
+  (resolve, reject) => {
+    async function loadModule() {
+      const module = await import(
+        "@unicode/unicode-17.0.0/Simple_Case_Mapping/Uppercase/code-points"
+      );
+      return module.default;
+    }
+
+    loadModule().then(resolve, reject);
+  },
+);
+
 export default function CharacterInfo() {
   const characterNames = use(characterNamesPromise);
   const characterCategories = use(characterCategoriesPromise);
   const bidiClasses = use(bidiClassesPromise);
+  const lowercaseMapping = use(lowercaseMappingPromise);
+  const uppercaseMapping = use(uppercaseMappingPromise);
+
   const codePoint = useCodePoint() ?? 0;
   const [, setSource] = useSource();
 
@@ -64,6 +93,9 @@ export default function CharacterInfo() {
   const codePointDescription = `U+${codePointAsHexa}`;
   const codePointLink = `https://www.compart.com/en/unicode/${codePointDescription}`;
   // const codePointLink = `https://symbl.cc/en/${codePointAsHexa}`;
+
+  const lowerCaseCodePoint = lowercaseMapping.get(codePoint);
+  const upperCaseCodePoint = uppercaseMapping.get(codePoint);
 
   const { enabledEncodings } = useDemoModeProps();
 
@@ -93,6 +125,20 @@ export default function CharacterInfo() {
           <ByteSequence codePoints={[codePoint]} encoding={encoding} />
         </Activity>
       ))}
+
+      {lowerCaseCodePoint && (
+        <>
+          <h3>Lowercase mapping</h3>
+          <CodePointDisplay codePoints={[lowerCaseCodePoint]} />
+        </>
+      )}
+
+      {upperCaseCodePoint && (
+        <>
+          <h3>Uppercase mapping</h3>
+          <CodePointDisplay codePoints={[upperCaseCodePoint]} />
+        </>
+      )}
 
       <p>
         <a href={codePointLink} rel="noopener" target="_blank">
